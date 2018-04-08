0762d793e0931a6ca89d0a4668acf5ac176f4e9f
Laurence Fong <laurencf@usc.edu>
2018-04-08 02:04:16 -0700

    data files updated

--- profArr.js
+++ profArr.js
@@ -1,20 +1,30 @@
-var rSorted = ["Yells \"fight on\" at a tour guide","Gets blocked by college tour group",
-"Skateboarder falls off board", "Bike gets stolen", 
-"\"Thief stole the tires of my bike\"","Starts an essay at midnight",
-"Marshall snakes","Does anyone have a printer I can use?",
-"Walks in class 5 min late","Walks in class 10 min late",
+var rSorted = ["Yells \"fight on\" at a tour guide","Eats cereal for dinner",
+"I'm going to go to class this year","Starts an essay at midnight",
+"Marshall snakes","Diversity :))",
+"Walks into class 10 min late",
 "Walks in 30 min late for 50 min class","Sleeps thru midterms",
-"Misses class completely","Loses ID card",
-"Leaves board in cafeteria","Caterpillar in EVK",
+"Misses class completely","Gets a C in WRIT 150",
+"Finds a caterpillar in EVK","3 textbooks for $1029.30",
 "Gets 3 hrs of sleep","Pulls an all-nighter",
 "Pulls an all-nighter in Leavey","Is there free food?",
 "If the professor is 15 min late, we're allowed to go","Premed drops",
-"Hasn't seen a vegetable in days","Cup o noodle",
+"Hasn't seen a vegetable in days","Subsists off of cup o noodle",
 "A shower is but a distant dream","Shakespear",
-"Daddy nikias","Sleeps thru 11am",
+"Daddy Nikias","Sleeps thru 11am",
 "Never goes to 8am","\"I'm gonna be wholesome this year\"",
 "\"I'm gonna be wholesome this year\"","Cooks a whole meal in the microwave",
 "Do you have an internship for the summer?","Applies to 100 internships, rejected from all of them",
-"\"I'm so busy\" *watches Netflix instead","Writes an essay on a book you didn't read",
-"Pointy things", "Diversity"   
+"\"I'm soo busy\" *watches Netflix instead","Writes an essay on a book you didn't read",
+"Pointy things", "Lectures gets cancelled!!",
+"Europe changed my life","What do you want to do with your life?",
+"Posts pics to make friends back home jealous"
+
+// ""
+
+// "Agonizes over an email to a professor for an hour for a one-word reply",""
+/**
+	"*Looks down for 2 secs \"What's going on?\""
+
+*/
+
 ];
