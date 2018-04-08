43ed21825f6a96307fbf432595b3e514000725d8
Laurence Fong <laurencf@usc.edu>
2018-04-08 01:59:30 -0700

    moved data out of the random sort js file

---
+++ studentArr.js
@@ -0,0 +1,26 @@
+var rSorted = ["Yells \"fight on\" at a tour guide","Eats cereal for dinner",
+"\"Thief stole the tires of my bike\"","Starts an essay at midnight",
+"Marshall snakes","Diversity :))",
+"Walks in class 5 min late","Walks in class 10 min late",
+"Walks in 30 min late for 50 min class","Sleeps thru midterms",
+"Misses class completely","Gets a C in WRIT 150",
+"Finds a caterpillar in EVK","3 textbooks for $1029.30",
+"Gets 3 hrs of sleep","Pulls an all-nighter",
+"Pulls an all-nighter in Leavey","Is there free food?",
+"If the professor is 15 min late, we're allowed to go","Premed drops",
+"Hasn't seen a vegetable in days","Cup o noodle",
+"A shower is but a distant dream","Shakespear",
+"Daddy Nikias","Sleeps thru 11am",
+"Never goes to 8am","\"I'm gonna be wholesome this year\"",
+"\"I'm gonna be wholesome this year\"","Cooks a whole meal in the microwave",
+"Do you have an internship for the summer?","Applies to 100 internships, rejected from all of them",
+"\"I'm so busy\" *watches Netflix instead","Writes an essay on a book you didn't read",
+"Pointy things", 
+"Europe changed my life","What do you want to do with your life?",
+"Posts pics to make friends back home jealous"
+
+// ""
+
+// "Agonizes over an email to a professor for an hour for a one-word reply",""
+
+];
