58fa92810ac459308f631e6dceb37476dc15138f
Hockey913101 <podd@usc.edu>
2018-04-07 17:12:43 -0700

    Add files via upload

---
+++ gameplay.js
@@ -0,0 +1,19 @@
+console.log("JavaScript is working");
+$(document).ready(function(){
+	console.log("The jquery is working!")
+
+	$("#img01").click(click)
+})
+
+var click = function(){
+	$(this).css("display", "block")
+
+}
+
+//checkwin(){
+
+//}
+
+//win(){
+
+//}
