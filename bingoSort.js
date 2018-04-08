c044d2148df9b14901fab68849401ed270fef4bf
Laurence Fong <laurencf@usc.edu>
2018-04-07 23:16:56 -0700

    Need to redo x functionality

--- bingoSort.js
+++ bingoSort.js
@@ -1,7 +1,4 @@
-
-
-<script>
-var points = ["0","1", "2", "3", "4", 
+var rSorted = ["0","1", "2", "3", "4", 
 "5","6","7","8","9",
 "10","11","12","13","14",
 "15","16","17","18","19",
@@ -10,8 +7,36 @@ var points = ["0","1", "2", "3", "4",
 // document.getElementById("hi").innerHTML = points;    
 
 
-function myFunction() {
-    points.sort(function(a, b){return 0.5 - Math.random()});
-    document.getElementById("hi").innerHTML = points[0];
+function randomize() {
+    rSorted.sort(function(a, b){return 0.5 - Math.random()});
 }
-</script>
+
+document.getElementById("data00").innerHTML = rSorted[0];
+document.getElementById("data01").innerHTML = rSorted[1];
+document.getElementById("data02").innerHTML = rSorted[2];
+document.getElementById("data03").innerHTML = rSorted[3];
+document.getElementById("data04").innerHTML = rSorted[4];
+
+document.getElementById("data10").innerHTML = rSorted[5];
+document.getElementById("data11").innerHTML = rSorted[6];
+document.getElementById("data12").innerHTML = rSorted[7];
+document.getElementById("data13").innerHTML = rSorted[8];
+document.getElementById("data14").innerHTML = rSorted[9];
+
+document.getElementById("data20").innerHTML = rSorted[10];
+document.getElementById("data21").innerHTML = rSorted[11];
+document.getElementById("data23").innerHTML = rSorted[12];
+document.getElementById("data24").innerHTML = rSorted[13];
+
+document.getElementById("data30").innerHTML = rSorted[14];
+document.getElementById("data31").innerHTML = rSorted[15];
+document.getElementById("data32").innerHTML = rSorted[16];
+document.getElementById("data33").innerHTML = rSorted[17];
+document.getElementById("data34").innerHTML = rSorted[18];
+
+document.getElementById("data40").innerHTML = rSorted[19];
+document.getElementById("data41").innerHTML = rSorted[20];
+document.getElementById("data42").innerHTML = rSorted[21];
+document.getElementById("data43").innerHTML = rSorted[22];
+document.getElementById("data44").innerHTML = rSorted[23];
+
