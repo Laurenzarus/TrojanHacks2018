0e959c91e70908b7d69979815bdd0f39f2e6e49e
Sean Vovan <svovan@usc.edu>
2018-04-08 10:59:00 -0700

    football content

--- ftBall.js
+++ ftBall.js
@@ -1,20 +1,20 @@
 var rSorted = ["Touchdown USC!","The Fight Song plays",
 "Victory bell goes ding-dong","I paid $5 for a water bottle :(",
-"Marching band cacophany","Darnold throws an interception)",
-"Torchis lit",//end of changed content
+"Marching band cacophany","Darnold throws an interception",
+"Torch is lit",//end of changed content
 "You see Traveler","Victory bell is rung",
 "Field goal","Fumble",
 "The entire Coliseum boos a bad call","Trojans win!",
 "Fan in a non-football jersey","Fan in an away team jersey",
-"A drunk fan fals on you","Someone is \"escorted out of the area\"",
+"A drunk fan falls on you","Someone is \"escorted out of the area\"",
 "Temperature is over 90 degrees","Someone says FUCLA",
 "The fight song plays","A flyover occurs",
 "You get the helmet shuffle correct","The hero of the game is called out",
 "The marching bands play over each other","You spot a hotdog vendor",
 "You kick the flagpole on the way to the Coliseum","Chug a \"non-water\" beverage before entrace",
-"Someone is in a song girl skirt,""Coliseum secuirty doesnt allow something to enter",
+"Someone is in a song girl skirt","Coliseum secuirty doesnt allow something to enter",
 "You sneak in more than two water bottles","Walk from field level to the top of the Coliseum",
-"Pay $5 for a water botle,"
+"Pay $5 for a water botle"
 
 // ""
 
