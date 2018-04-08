e42895bd21bf1024155ba7f2c1c0bd4a3d265fa4
Elizabeth Zhou <zhoue@usc.edu>
2018-04-08 01:09:18 -0700

    updated with entries

--- bingoSort.js
+++ bingoSort.js
@@ -1,11 +1,23 @@
-var rSorted = ["0","1", "2", "3", "4", 
-"5","6","7","8","9",
-"10","11","12","13","14",
-"15","16","17","18","19",
-"20","21","22","23","24",
-"25","26","27","28","29"];
-// document.getElementById("hi").innerHTML = points;    
-
+var rSorted = ["Yells \"fight on\" at a tour guide","Gets blocked by college tour group",
+"Skateboarder falls off board", "Bike gets stolen", 
+"\"Thief stole the tires of my bike\"","Starts an essay at midnight",
+"Marshall snakes","Does anyone have a printer I can use?",
+"Walks in class 5 min late","Walks in class 10 min late",
+"Walks in 30 min late for 50 min class","Sleeps thru midterms",
+"Misses class completely","Loses ID card",
+"Leaves board in cafeteria","Caterpillar in EVK",
+"Gets 3 hrs of sleep","Pulls an all-nighter",
+"Pulls an all-nighter in Leavey","Is there free food?",
+"If the professor is 15 min late, we're allowed to go","Premed drops",
+"Hasn't seen a vegetable in days","Cup o noodle",
+"A shower is but a distant dream","Shakespear",
+"Daddy nikias","Sleeps thru 11am",
+"Never goes to 8am","\"I'm gonna be wholesome this year\"",
+"\"I'm gonna be wholesome this year\"","Cooks a whole meal in the microwave",
+"Do you have an internship for the summer?","Applies to 100 internships, rejected from all of them",
+"\"I'm so busy\" *watches Netflix instead","Writes an essay on a book you didn't read",
+"Pointy things", "Diversity"   
+];
 
 function randomize() {
     rSorted.sort(function(a, b){return 0.5 - Math.random()});
